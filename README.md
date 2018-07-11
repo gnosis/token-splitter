59f6dae4da7c9f1a51a46bf0202d961d8d3c5df2
Uxio Fuentefria <uxio@gnosis.pm>
2018-07-11 18:09:28 +0200

    Deploy to rinkeby

---
+++ README.md
@@ -0,0 +1,7 @@
+Token splitter
+-------------
+
+This contract implements a function that receives a `list of addresses`, a `token contract address` and the `amount to send per account`
+and sends to every contract the amount of tokens.
+
+Rinkeby address: `0xe1a567b650BC37BCF395003477FaAdA944a7Ab36`
--- truffle.js
+++ truffle.js
@@ -1,16 +1,19 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
  *     gasPrice: 10000000000,
  *   },
  */
+
+const HDWalletProvider = require("truffle-hdwallet-provider-privkey")
+privKey = ''
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -20,6 +23,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 9545,
       network_id: "*"
+    },
+    rinkeby: {
+      provider: () => {
+        return new HDWalletProvider(privKey, "https://rinkeby.infura.io/gnosis")
+      },
+      network_id: "4"
     }
   }
 }
