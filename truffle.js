7b197201c370f4d7d71ce53caf732ce1d5cadd81
David Albela <3659067+davidalbela@users.noreply.github.com>
2019-12-27 17:09:35 +0100

    Update truffle configuration
      - Remove `truffle-config.js` file.
      - Update `HDWalletProvider` dependency.
      - Add token and mnemonic constants
      from environment variables.
      - Disable solc optimizer.

--- truffle.js
+++ truffle.js
@@ -1,19 +1,9 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() {
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
-const HDWalletProvider = require("truffle-hdwallet-provider-privkey")
-privKey = ''
+require('dotenv').config()
+
+const token = process.env.INFURA_TOKEN
+const mnemonic = process.env.MNEMONIC
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -21,14 +11,20 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 9545,
+      port: 8545,
       network_id: "*"
     },
     rinkeby: {
       provider: () => {
-        return new HDWalletProvider(privKey, "https://rinkeby.infura.io/gnosis")
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${token}`)
       },
-      network_id: "4"
+      network_id: "4",
+      gasPrice: 25000000000, // 25 Gwei
     }
-  }
-}
+  },
+  solc: {
+    optimizer: {
+      enabled: false
+    }
+  },
+};
