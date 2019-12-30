7aadd7850d85c171388c7dabc9b73729e2712176
davidalbela <3659067+davidalbela@users.noreply.github.com>
2019-12-30 12:39:34 +0100

    Update Solitiy to `v0.5.0`
      - Update pragma contracts code.
      - Update TokenGNO import to `GnosisStandardToken.sol`.

--- contracts/ForTestingOnly/TokenGNO.sol
+++ contracts/ForTestingOnly/TokenGNO.sol
@@ -1,9 +1,9 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.5.0 <0.7.0;
 
-import "@gnosis.pm/util-contracts/contracts/StandardToken.sol";
+import "@gnosis.pm/util-contracts/contracts/GnosisStandardToken.sol";
 
 
-contract TokenGNO is StandardToken {
+contract TokenGNO is GnosisStandardToken {
     string public constant symbol = "GNO";
     string public constant name = "GNO Test Token";
     uint8 public constant decimals = 18;
--- contracts/TokenSplitter.sol
+++ contracts/TokenSplitter.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.24;
+pragma solidity >=0.5.0 <0.7.0;
 
 import "@gnosis.pm/util-contracts/contracts/Token.sol";
 
 
 contract TokenSplitter {
-    function splitTokens(address[] _recipients, address _tokenAddress, uint _amountForEach) public {
+    function splitTokens(address[] memory _recipients, address _tokenAddress, uint _amountForEach) public {
         Token erc20Token = Token(_tokenAddress);
         for (uint i = 0; i < _recipients.length; i++) {
             erc20Token.transferFrom(msg.sender, _recipients[i], _amountForEach);
